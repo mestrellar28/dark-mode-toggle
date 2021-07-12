0e173a3592952305f3c5c2dcbc0b85da3fbf8ccb
Manuel Estrella <mestrellar28@gmail.com>
2021-07-12 16:33:13 +0200

    add the navbar, and cards structure to html, style the navbar, darkmode button and cards, add the script to switch the dark class in body and active class in darkmode button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './main.scss';
+
+const btnToggle = document.querySelector('#darkmode-toggle');
+
+// listen to darkmode button when is clicked
+btnToggle.addEventListener('click', () => {
+   // toggle the dark class in body tag
+   document.body.classList.toggle('dark');
+   // toggle the active class in darkmode button
+   btnToggle.classList.toggle('active');
+});
